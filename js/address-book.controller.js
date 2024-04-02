18e15dab005945df3d46d21f22762c701732faee
Darr Or <darrmorgan@gmail.com>
2024-04-02 14:00:48 -0400

    Setup all relevant files for movies exercise

--- js/address-book.controller.js
+++ js/address-book.controller.js
@@ -10,29 +10,30 @@ function renderCards(data) {
    
     const strHtml = data.map((card, idx) => `
                 <div class = card card${idx+1}>
-                    <div>${card.fname}&nbsp${card.lname}</div> 
+                    <h2>${card.fname} ${card.lname}</h2> 
                     <img src="https://robohash.org/{${idx+1}}?set=set5" alt="">
-                    <li>
-                        <span>Phone:</span>
-                        <span> ${card.tel}</span>
-                    </li> 
-                    <li>
-                        <span>City:</span>
-                        <span> ${card.city}</span>
-                    </li> 
-                    <li>
-                        <span>State:</span>
-                        <span> ${card.state}</span>
-                    </li> 
-                    <li>
-                        <span>Zip:</span>
-                        <span> ${card.zip}</span>
-                    </li> 
-                    <li>
-                        <span>Address:</span>
-                        <span> ${card.address}</span>
-                    </li> 
-                    
+                    <div class="card-text">
+                        <li>
+                            <span>Phone:</span>
+                            <span> ${card.tel}</span>
+                        </li> 
+                        <li>
+                            <span>City:</span>
+                            <span> ${card.city}</span>
+                        </li> 
+                        <li>
+                            <span>State:</span>
+                            <span> ${card.state}</span>
+                        </li> 
+                        <li>
+                            <span>Zip:</span>
+                            <span> ${card.zip}</span>
+                        </li> 
+                        <li>
+                            <span>Address:</span>
+                            <span> ${card.address}</span>
+                        </li>
+                    </div>   
                 </div>`
                 ).join('')
 
