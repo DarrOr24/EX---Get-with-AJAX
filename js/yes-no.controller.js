c4df3ebafc622f7c2020860d0ccb6d0b07c019db
Darr Or <darrmorgan@gmail.com>
2024-04-03 09:23:37 -0400

    Get answer js and css

--- js/yes-no.controller.js
+++ js/yes-no.controller.js
@@ -1,1 +1,17 @@
 'use strict'
+
+function onInit() {
+    const elInput = document.querySelector('input')
+    elInput.addEventListener('input', debounce(onAsk, 1000))
+}
+
+function onAsk() {
+	getAns(renderAns)
+}
+
+function renderAns(ans) {
+    console.log(ans)
+
+    document.querySelector('.answer h2').innerText = ans.answer
+    document.querySelector('.answer img').src = ans.image
+}
