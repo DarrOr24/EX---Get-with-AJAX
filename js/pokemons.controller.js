aa2d9879ff05ecffc819c16aec0e7c980d985ab8
Darr Or <darrmorgan@gmail.com>
2024-04-03 18:01:46 -0400

    Finish pokemon plus animation

--- js/pokemons.controller.js
+++ js/pokemons.controller.js
@@ -1,1 +1,52 @@
 'use strict'
+
+function onInit(){
+    getPokemons(renderPokemons)
+}
+
+function renderPokemons(data){
+    if(gCachePokemons.length === 0){
+        savePokemonCache(data.results)
+        data = data.results
+    }
+
+    console.log(data)
+
+    data.forEach(pokemon => {
+        getPoke(renderPokeInfo, pokemon.url)
+    })
+
+    
+    const sortedData = gCachePokemons.slice()
+    sortPokemonsList(sortedData)
+
+    setInterval(()=> {
+        renderPokemonCards(sortedData, 'back_default')
+        setTimeout(renderPokemonCards, 500, sortedData, 'back_shiny')
+        setTimeout(renderPokemonCards, 1000, sortedData, 'front_default')
+        
+    }, 1500)
+
+    
+    
+}
+
+function renderPokemonCards(sortedData, key='front_default'){
+    const strHtml = sortedData.map(pokemon => `
+        <div class="pokemon-card">
+            <div>${pokemon.name}</div>
+            <div>Weight: ${pokemon.weight}</div>
+            <img src="${pokemon.imgs[key]}" alt="">
+        </div>
+    `).join('')
+
+    document.querySelector('.pokemons-list').innerHTML = strHtml
+}
+
+function renderPokeInfo(pokemon){
+    console.log(pokemon.sprites)
+    gCachePokemons[pokemon.id - 1].weight = pokemon.weight 
+    gCachePokemons[pokemon.id - 1].imgs = pokemon.sprites 
+    savePokemonCache(gCachePokemons)
+    
+}
--- js/services/pokemons.service.js
+++ js/services/pokemons.service.js
@@ -1,1 +1,31 @@
 'use strict'
+
+var gCachePokemons = loadFromStorage('pokemonsCache') || []
+
+
+function getPokemons(onSuccess) {
+
+    if(gCachePokemons.length > 0){
+        console.log('from cache...')
+        onSuccess(gCachePokemons)
+        return 
+    }
+
+    console.log('from network...')
+    $.get('https://pokeapi.co/api/v2/pokemon/', onSuccess)
+}
+
+function savePokemonCache(data) {
+    gCachePokemons = data
+    saveToStorage('pokemonsCache', gCachePokemons)
+}
+
+function sortPokemonsList(pokemonsList){
+    return pokemonsList.sort((pokemon1, pokemon2) => pokemon1.name.localeCompare(pokemon2.name) )
+ 
+}
+
+function getPoke(onSuccess, url){
+    $.get(url,onSuccess)
+}
+
